e6a69d8fd5485859f372a502572f3ccc0a63c19d
Kade Perrotti <kp5431@g.rit.edu>
2019-09-10 21:43:19 -0400

    added gradle garbage

--- app/src/main/java/com/yut/originalqualityphotoshare/MainActivity.java
+++ app/src/main/java/com/yut/originalqualityphotoshare/MainActivity.java
@@ -1,6 +1,7 @@
 package com.yut.originalqualityphotoshare;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
